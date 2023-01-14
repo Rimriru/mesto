c5799eb2012d64654ca6242cd24907f99c4f986a
Rimriru <ororonya@gmail.com>
2023-01-14 17:23:07 +0200

    fix: file system refactoring

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 // To open and close popup by edit button and cross
 
-let editBtn = document.querySelector('.edit-button');
+let editBtn = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
 let closePopupBtn = document.querySelector('.popup__close-button');
 
@@ -17,9 +17,9 @@ function closePopup() {
 
 // To submit the popup form and save values of name and description from user input
 
-let profileName = document.querySelector('.profile-info__name');
+let profileName = document.querySelector('.profile__name');
 let nameInput = document.querySelector('.popup__person_input_name');
-let profileDescription = document.querySelector('.profile-info__description');
+let profileDescription = document.querySelector('.profile__description');
 let descriptionInput = document.querySelector('.popup__person_input_description');
 let formPopupElement = document.querySelector('.popup__form');
 
