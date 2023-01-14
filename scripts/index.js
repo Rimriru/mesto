f47da8d88584c2462a866a43799e69eaa322247f
Rimriru <ororonya@gmail.com>
2023-01-14 22:53:40 +0200

    fix: adaptive, js

--- scripts/index.js
+++ scripts/index.js
@@ -4,11 +4,16 @@ let editBtn = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
 let closePopupBtn = document.querySelector('.popup__close-button');
 
-editBtn.addEventListener('click', openPopup);
-closePopupBtn.addEventListener('click', closePopup);
+let profileName = document.querySelector('.profile__name');
+let nameInput = document.querySelector('.popup__person_input_name');
+let profileDescription = document.querySelector('.profile__description');
+let descriptionInput = document.querySelector('.popup__person_input_description');
+let formPopupElement = document.querySelector('.popup__form'); 
 
 function openPopup() {
   popup.classList.add('popup_opened');
+  nameInput.value = profileName.textContent;
+  descriptionInput.value = profileDescription.textContent;
 }
 
 function closePopup() {
@@ -17,17 +22,12 @@ function closePopup() {
 
 // To submit the popup form and save values of name and description from user input
 
-let profileName = document.querySelector('.profile__name');
-let nameInput = document.querySelector('.popup__person_input_name');
-let profileDescription = document.querySelector('.profile__description');
-let descriptionInput = document.querySelector('.popup__person_input_description');
-let formPopupElement = document.querySelector('.popup__form');
+editBtn.addEventListener('click', openPopup);
+closePopupBtn.addEventListener('click', closePopup);
 
 formPopupElement.addEventListener('submit', function(event) {
   event.preventDefault();
-  let name = nameInput.value;
-  let description = descriptionInput.value;
-  profileName.textContent = `${name}`;
-  profileDescription.textContent = `${description}`;
+  profileName.textContent = `${nameInput.value}`;
+  profileDescription.textContent = `${descriptionInput.value}`;
   closePopup();
 });
